fcb0b1128039a0002ec4a2f400feca132bf455f4
mkslofstra <mariska-karina@hotmail.com>
2019-03-08 16:28:53 +0100

    Get pheno counts based on region

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
+Vue.component('VueSlider', VueSlider)
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -1,16 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@molgenis/molgenis-api-client'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    phenotypes: [],
+    array: [],
+    overlap: 0,
+    patients: []
   },
   mutations: {
-
+    setPatients (state, items) {
+      state.patients = items
+    },
+    setPhenotypes (state, attrs) {
+      const phenotypes = attrs.filter((attr) => {
+        if (attr.fieldType === 'CATEGORICAL' && attr.name !== 'sex' && attr.refEntity.label === 'bool_options') {
+          return true
+        } else {
+          return false
+        }
+      }).map((phenotypeAttr) => {
+        return { value: phenotypeAttr.name, text: phenotypeAttr.label }
+      })
+      state.phenotypes = phenotypes
+    },
+    setArrayData (state, items) {
+      state.array = items
+    }
+  },
+  getters: {
+    getPhenotypes: (state) => state.phenotypes
   },
   actions: {
-
+    getPhenotypeOptions ({ commit }) {
+      return api.get('api/v2/c6_research_patients').then((response) => {
+        commit('setPhenotypes', response.meta.attributes)
+      })
+    },
+    getPatients ({ commit }) {
+      return api.get('api/v2/c6_research_patients?num=10000').then((response) => {
+        commit('setPatients', response.items)
+      })
+    },
+    getArrayData ({ commit }) {
+      return api.get('api/v2/c6_array?num=10000').then((response) => {
+        commit('setArrayData', response.items)
+      })
+    }
   }
 })
